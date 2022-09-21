f2ba29f15d55f1cbb99546c3c89a9f3c3bcf6694
Dom Lawlor <domlawlor1@gmail.com>
2022-09-22 03:04:52 +1000

    bomb view set to not visible on restart game

--- Main.gd
+++ Main.gd
@@ -10,6 +10,7 @@ var bombPuzzleScene = preload("res://BombPuzzle.tscn")
 onready var debugMenu = $DebugLayer/Menu
 onready var main_2d : Node2D = $Main2D
 onready var puzzle_2d : Node2D = $UILayer/Puzzle2D
+onready var BombPuzzle : Node2D = $UILayer/BombPuzzle
 onready var bombTimer : Node2D = $UILayer/BombTimer
 onready var paletteShader = $TopLayer/PaletteShader
 
@@ -138,6 +139,7 @@ func _on_LoadPuzzle48_pressed():
 func _on_fade_to_dark_complete():
 	if Global.state == STATE.CHANGING_LEVEL:
 		bombTimer.visible = true # needed for coming from titles
+		BombPuzzle.visible = false
 		var levelExitedNum = current_level_num
 		current_level_num += 1
 	
