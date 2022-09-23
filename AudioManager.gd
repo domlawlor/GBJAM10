0a1665013814d91a4357d1261a16ffab0ede3390
Tim Day <timday888@gmail.com>
2022-09-23 21:02:36 +1000

    first pass music

--- AudioManager.gd
+++ AudioManager.gd
@@ -1,18 +1,27 @@
 extends Node2D
 
+var STATE = Global.State
+
 onready var Explosion = $Explosion
 onready var Defuse = $Defuse
 onready var Cut = $Cut
+onready var GameplayNormal = $GameplayNormal
 
 func _ready():
 	Events.connect("play_audio", self, "_on_play_audio")
 	Events.connect("wire_cut", self, "_on_wire_cut")
 	Events.connect("bomb_explode", self, "_on_bomb_explode")
+	Events.connect("fade_from_dark_complete", self, "_on_fade_from_dark_complete")
 	
 func _exit():
 	Events.disconnect("play_audio", self, "_on_play_audio")
 	Events.disconnect("wire_cut", self, "_on_wire_cut")
 	Events.disconnect("bomb_explode", self, "_on_bomb_explode")
+	Events.disconnect("fade_from_dark_complete", self, "_on_fade_from_dark_complete")
+
+func PlayGameplayNormal():
+	if !GameplayNormal.playing:
+		GameplayNormal.play()
 
 func _on_play_audio(name):
 	match name:
@@ -27,4 +36,10 @@ func _on_wire_cut():
 	Cut.play()
 	
 func _on_bomb_explode():
+	GameplayNormal.stop()
 	Explosion.play()
+
+func _on_fade_from_dark_complete():
+	print("gggg-" + str(Global.state))
+	if Global.state == STATE.CHANGING_LEVEL:
+		PlayGameplayNormal()
