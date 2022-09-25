6b207d4688082af93c13606f1e44a88738309511
Tim Day <timday888@gmail.com>
2022-09-25 22:48:30 +1000

    palette updates

--- PaletteShader.gd
+++ PaletteShader.gd
@@ -6,7 +6,7 @@ onready var explodeTimer : Timer = $ExplodeTimer
 onready var titleExplodeTimer : Timer = $TitleExplodeTimer
 onready var winExplodeTimer : Timer = $WinExplodeTimer
 
-export var DefaultColorPaletteIndex : int = 2 
+export var DefaultColorPaletteIndex : int = 1
 
 var List = [
 	{
@@ -15,6 +15,13 @@ var List = [
 		"dark" : { "r" : 48, "g" : 98, "b" : 48 },
 		"light" : { "r" : 139, "g" : 172, "b" : 15 },
 		"lightest" : { "r" : 155, "g" : 188, "b" : 15 },
+	},	
+	{
+		"name" : "green_with_more_contrast",
+		"darkest" : { "r" : 0, "g" : 19, "b" : 26 },
+		"dark" : { "r" : 61, "g" : 128, "b" : 38 },
+		"light" : { "r" : 171, "g" : 204, "b" : 71 },
+		"lightest" : { "r" : 250, "g" : 255, "b" : 179 },
 	},
 	{
 		"name" : "demichrome",
@@ -24,27 +31,6 @@ var List = [
 		"lightest" : { "r" : 233, "g" : 239, "b" : 236 },
 	},
 	{
-		"name" : "green_with_more_contrast",
-		"darkest" : { "r" : 0, "g" : 19, "b" : 26 },
-		"dark" : { "r" : 61, "g" : 128, "b" : 38 },
-		"light" : { "r" : 171, "g" : 204, "b" : 71 },
-		"lightest" : { "r" : 250, "g" : 255, "b" : 179 },
-	},
-	{
-		"name" : "ice_cream",
-		"darkest" : { "r" : 124, "g" : 63, "b" : 88 },
-		"dark" : { "r" : 235, "g" : 107, "b" : 111 },
-		"light" : { "r" : 249, "g" : 168, "b" : 117 },
-		"lightest" : { "r" : 255, "g" : 246, "b" : 211 },
-	},
-	{
-		"name" : "wish",
-		"darkest" : { "r" : 98, "g" : 46, "b" : 76 },
-		"dark" : { "r" : 117, "g" : 80, "b" : 232 },
-		"light" : { "r" : 96, "g" : 143, "b" : 207 },
-		"lightest" : { "r" : 139, "g" : 229, "b" : 255 },
-	},
-	{
 		"name" : "crimson",
 		"darkest" : { "r" : 27, "g" : 3, "b" : 38 },
 		"dark" : { "r" : 122, "g" : 28, "b" : 75 },
@@ -52,11 +38,32 @@ var List = [
 		"lightest" : { "r" : 239, "g" : 249, "b" : 214 },
 	},
 	{
-		"name" : "galactic_pizza",
-		"darkest" : { "r" : 58, "g" : 0, "b" : 65 },
-		"dark" : { "r" : 196, "g" : 119, "b" : 162 },
-		"light" : { "r" : 242, "g" : 241, "b" : 139 },
-		"lightest" : { "r" : 255, "g" : 255, "b" : 255 },
+		"name" : "matrix",
+		"darkest" : { "r" : 13, "g" : 26, "b" : 26 },
+		"dark" : { "r" : 91, "g" : 140, "b" : 124 },
+		"light" : { "r" : 173, "g" : 217, "b" : 188 },
+		"lightest" : { "r" : 242, "g" : 255, "b" : 242 },
+	},
+	{
+		"name" : "pumpkin",
+		"darkest" : { "r" : 0x14, "g" : 0x2b, "b" : 0x23 },
+		"dark" : { "r" : 0x19, "g" : 0x69, "b" : 0x2c },
+		"light" : { "r" : 0xe0, "g" : 0x6e, "b" : 0x16 },
+		"lightest" : { "r" : 0xf7, "g" : 0xdb, "b" : 0x7e },
+	},
+	{
+		"name" : "4seas",
+		"darkest" : { "r" : 0x00, "g" : 0x00, "b" : 0x00 },
+		"dark" : { "r" : 0x24, "g" : 0x22, "b" : 0x36 }, # 222034
+		"light" : { "r" : 0x5b, "g" : 0x6e, "b" : 0xe1 },
+		"lightest" : { "r" : 0xcb, "g" : 0xdb, "b" : 0xfc },
+	},
+	{
+		"name" : "blood-crow",
+		"darkest" : { "r" : 0x19, "g" : 0x00, "b" : 0x00 },
+		"dark" : { "r" : 0x56, "g" : 0x09, "b" : 0x09 },
+		"light" : { "r" : 0xad, "g" : 0x20, "b" : 0x20 },
+		"lightest" : { "r" : 0xf2, "g" : 0xe6, "b" : 0xe6 },
 	},
 	{
 		"name" : "cherry_melon",
@@ -73,11 +80,32 @@ var List = [
 		"lightest" : { "r" : 255, "g" : 209, "b" : 145 },
 	},
 	{
-		"name" : "matrix",
-		"darkest" : { "r" : 13, "g" : 26, "b" : 26 },
-		"dark" : { "r" : 91, "g" : 140, "b" : 124 },
-		"light" : { "r" : 173, "g" : 217, "b" : 188 },
-		"lightest" : { "r" : 242, "g" : 255, "b" : 242 },
+		"name" : "ice_cream",
+		"darkest" : { "r" : 90, "g" : 50, "b" : 60 }, #124 63 88
+		"dark" : { "r" : 235, "g" : 107, "b" : 111 },
+		"light" : { "r" : 249, "g" : 168, "b" : 117 },
+		"lightest" : { "r" : 255, "g" : 246, "b" : 211 },
+	},
+	{
+		"name" : "wish",
+		"darkest" : { "r" : 70, "g" : 30, "b" : 50 }, #98 46 76
+		"dark" : { "r" : 117, "g" : 80, "b" : 210 }, #117 80 232
+		"light" : { "r" : 96, "g" : 143, "b" : 207 },
+		"lightest" : { "r" : 139, "g" : 229, "b" : 255 },
+	},
+	{
+		"name" : "galactic_pizza",
+		"darkest" : { "r" : 58, "g" : 0, "b" : 65 },
+		"dark" : { "r" : 196, "g" : 119, "b" : 162 },
+		"light" : { "r" : 242, "g" : 241, "b" : 139 },
+		"lightest" : { "r" : 255, "g" : 255, "b" : 255 },
+	},
+	{
+		"name" : "dusty4",
+		"darkest" : { "r" : 0x37, "g" : 0x2a, "b" : 0x51 },
+		"dark" : { "r" : 0x3a, "g" : 0x50, "b" : 0x68 },
+		"light" : { "r" : 0x5a, "g" : 0x8f, "b" : 0x78 },
+		"lightest" : { "r" : 0xf5, "g" : 0xf6, "b" : 0xdf },
 	},
 #	{
 #		"name" : "barbie",
@@ -93,20 +121,6 @@ var List = [
 #		"light" : { "r" : 0x3d, "g" : 0xff, "b" : 0x98 },
 #		"lightest" : { "r" : 0xf0, "g" : 0xf6, "b" : 0xf0 },
 #	},
-	{
-		"name" : "blood-crow",
-		"darkest" : { "r" : 0x19, "g" : 0x00, "b" : 0x00 },
-		"dark" : { "r" : 0x56, "g" : 0x09, "b" : 0x09 },
-		"light" : { "r" : 0xad, "g" : 0x20, "b" : 0x20 },
-		"lightest" : { "r" : 0xf2, "g" : 0xe6, "b" : 0xe6 },
-	},
-	{
-		"name" : "dusty4",
-		"darkest" : { "r" : 0x37, "g" : 0x2a, "b" : 0x51 },
-		"dark" : { "r" : 0x3a, "g" : 0x50, "b" : 0x68 },
-		"light" : { "r" : 0x5a, "g" : 0x8f, "b" : 0x78 },
-		"lightest" : { "r" : 0xf5, "g" : 0xf6, "b" : 0xdf },
-	},
 #	{
 #		"name" : "fiery-plague",
 #		"darkest" : { "r" : 0x1a, "g" : 0x21, "b" : 0x29 },
@@ -121,20 +135,13 @@ var List = [
 #		"light" : { "r" : 0x14, "g" : 0xe1, "b" : 0xff },
 #		"lightest" : { "r" : 0xff, "g" : 0x7a, "b" : 0xd6 },
 #	},
-	{
-		"name" : "pumpkin",
-		"darkest" : { "r" : 0x14, "g" : 0x2b, "b" : 0x23 },
-		"dark" : { "r" : 0x19, "g" : 0x69, "b" : 0x2c },
-		"light" : { "r" : 0xe0, "g" : 0x6e, "b" : 0x16 },
-		"lightest" : { "r" : 0xf7, "g" : 0xdb, "b" : 0x7e },
-	},
-	{
-		"name" : "red-blood",
-		"darkest" : { "r" : 0x12, "g" : 0x0a, "b" : 0x19 },
-		"dark" : { "r" : 0x5e, "g" : 0x40, "b" : 0x69 },
-		"light" : { "r" : 0x7e, "g" : 0x1f, "b" : 0x23 },
-		"lightest" : { "r" : 0xc4, "g" : 0x18, "b" : 0x1f },
-	},
+#	{
+#		"name" : "red-blood",
+#		"darkest" : { "r" : 0x12, "g" : 0x0a, "b" : 0x19 },
+#		"dark" : { "r" : 0x5e, "g" : 0x40, "b" : 0x69 },
+#		"light" : { "r" : 0x7e, "g" : 0x1f, "b" : 0x23 },
+#		"lightest" : { "r" : 0xc4, "g" : 0x18, "b" : 0x1f },
+#	},
 ]
 
 var DEF_DARKEST : Color
